a52818b2a37d4d3200958251dcde0af4a897d16e
Gessica <santos.gessicacarolina@gmail.com>
2023-04-24 16:51:43 -0300

    Transactions and homepage done

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,73 +1,107 @@
-import styled from "styled-components"
-import { BiExit } from "react-icons/bi"
-import { AiOutlineMinusCircle, AiOutlinePlusCircle } from "react-icons/ai"
-import { useContext } from "react"
-import { UserContext } from "../contexts/UserContext"
-import { useNavigate } from "react-router-dom"
+import styled from "styled-components";
+import { BiExit } from "react-icons/bi";
+import { AiOutlineMinusCircle, AiOutlinePlusCircle } from "react-icons/ai";
+import { useContext, useEffect, useState } from "react";
+import { UserContext } from "../contexts/UserContext";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export default function HomePage() {
-  const {user, setUser} = useContext(UserContext);
+  const { user, setUser } = useContext(UserContext);
+  const [transactions, setTransactions] = useState([]);
+  const [total, setTotal] = useState(0);
   const navigate = useNavigate();
 
-  function handleLogOut(){
-    localStorage.removeItem('user');
+  useEffect(() => {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${user.token}`,
+      },
+    };
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/transactions/`, config)
+      .then((res) => {
+        const trans = res.data.reverse();
+        setTransactions(trans);
+        const totalAmount = trans.reduce((value, t) => {
+          return t.type === "entrada" ? value + t.amount : value - t.amount;
+        }, 0);
+        setTotal(totalAmount);
+      })
+      .catch((err) => {
+        if (err.response) {
+          alert(err.response.data);
+        }
+      });
+  }, []);
+
+  console.log(total);
+
+  function handleLogOut() {
+    localStorage.removeItem("user");
     setUser({});
-    navigate('/');
+    navigate("/");
   }
+
+  console.log(transactions);
 
   return (
     <HomeContainer>
       <Header>
         <h1>Olá, {user.name}</h1>
-        <BiExit style={{cursor:"pointer"}} onClick={handleLogOut}/>
+        <BiExit style={{ cursor: "pointer" }} onClick={handleLogOut} />
       </Header>
 
       <TransactionsContainer>
-        <ul>
-          <ListItemContainer>
-            <div>
-              <span>30/11</span>
-              <strong>Almoço mãe</strong>
-            </div>
-            <Value color={"negativo"}>120,00</Value>
-          </ListItemContainer>
-
-          <ListItemContainer>
-            <div>
-              <span>15/11</span>
-              <strong>Salário</strong>
-            </div>
-            <Value color={"positivo"}>3000,00</Value>
-          </ListItemContainer>
-        </ul>
-
-        <article>
-          <strong>Saldo</strong>
-          <Value color={"positivo"}>2880,00</Value>
-        </article>
+        {transactions.length === 0 ? (
+          <h3>Não há registros de entrada ou saída</h3>
+        ) : (
+          <>
+            <ul>
+              {transactions.map((t, id) => (
+                <ListItemContainer key={id}>
+                  <div>
+                    <span>{t.date}</span>
+                    <strong>{t.description}</strong>
+                  </div>
+                  <Value color={t.type === "saida" ? "negativo" : "positivo"}>
+                    {t.amount.toFixed(2)}
+                  </Value>
+                </ListItemContainer>
+              ))}
+            </ul>
+            <article>
+              <strong>Saldo</strong>
+              <Value color={total > 0 ? "positivo" : "negativo"}>{total.toFixed(2)}</Value>
+            </article>
+          </>
+        )}
       </TransactionsContainer>
 
-
       <ButtonsContainer>
-        <button onClick={() => navigate('/nova-transacao/entrada')}>
+        <button onClick={() => navigate("/nova-transacao/entrada")}>
           <AiOutlinePlusCircle />
-          <p>Nova <br /> entrada</p>
+          <p>
+            Nova <br /> entrada
+          </p>
         </button>
-        <button onClick={() => navigate('/nova-transacao/saida')}>
+        <button onClick={() => navigate("/nova-transacao/saida")}>
           <AiOutlineMinusCircle />
-          <p>Nova <br />saída</p>
+          <p>
+            Nova <br />
+            saída
+          </p>
         </button>
       </ButtonsContainer>
-
     </HomeContainer>
-  )
+  );
 }
 
 const HomeContainer = styled.div`
   display: flex;
   flex-direction: column;
   height: calc(100vh - 50px);
-`
+`;
 const Header = styled.header`
   display: flex;
   align-items: center;
@@ -76,7 +110,7 @@ const Header = styled.header`
   margin-bottom: 15px;
   font-size: 26px;
   color: white;
-`
+`;
 const TransactionsContainer = styled.article`
   flex-grow: 1;
   background-color: #fff;
@@ -86,21 +120,29 @@ const TransactionsContainer = styled.article`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  h3 {
+    display:flex;
+    justify-content: center;
+    margin-top:20%;
+    color:#868686;
+    font-size:20px;
+  }
   article {
     display: flex;
-    justify-content: space-between;   
+    justify-content: space-between;
     strong {
       font-weight: 700;
       text-transform: uppercase;
     }
   }
-`
+`;
+
 const ButtonsContainer = styled.section`
   margin-top: 15px;
   margin-bottom: 0;
   display: flex;
   gap: 15px;
-  
+
   button {
     width: 50%;
     height: 115px;
@@ -113,12 +155,12 @@ const ButtonsContainer = styled.section`
       font-size: 18px;
     }
   }
-`
+`;
 const Value = styled.div`
   font-size: 16px;
   text-align: right;
   color: ${(props) => (props.color === "positivo" ? "green" : "red")};
-`
+`;
 const ListItemContainer = styled.li`
   display: flex;
   justify-content: space-between;
@@ -130,4 +172,4 @@ const ListItemContainer = styled.li`
     color: #c6c6c6;
     margin-right: 10px;
   }
-`
+`;
--- src/pages/TransactionPage.js
+++ src/pages/TransactionPage.js
@@ -1,13 +1,50 @@
+import axios from "axios";
+import { useContext, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import styled from "styled-components"
+import { UserContext } from "../contexts/UserContext";
 
 export default function TransactionsPage() {
+  const {user} = useContext(UserContext);
+  const {tipo} = useParams();
+  const [amount, setAmount] = useState('');
+  const [description, setDescription] = useState('');
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  function sendTransaction(e) {
+    e.preventDefault();
+    setLoading(true);
+    const config = {
+      headers: {
+        Authorization: `Bearer ${user.token}`,
+      },
+    };
+    if (isNaN(amount)) {
+      setLoading(false);
+      return alert("Valor inválido, por favor coloque apenas números");
+    }
+    const body = { description, amount: Number(amount) };
+    axios
+      .post(`${process.env.REACT_APP_API_URL}/transactions/${tipo}`, body, config)
+      .then((res) => {
+        console.log(res.data);
+        navigate("/home");
+      })
+      .catch((err) => {
+        console.log(err.response)
+        alert(err.response.data);
+      })
+      .finally(() => setLoading(false));
+  }
+
   return (
     <TransactionsContainer>
-      <h1>Nova TRANSAÇÃO</h1>
-      <form>
-        <input placeholder="Valor" type="text"/>
-        <input placeholder="Descrição" type="text" />
-        <button>Salvar TRANSAÇÃO</button>
+      <h1>Nova {tipo === 'saida' ? "Saída" : "Entrada"}</h1>
+      <form onSubmit={sendTransaction}>
+        <input placeholder="Valor" type="text" required disabled={loading} value={amount} onChange={(e) => setAmount(e.target.value)}/>
+        <input placeholder="Descrição" type="text" required disabled={loading} value={description} onChange={(e) => setDescription(e.target.value)}/>
+        <button>Salvar {tipo === 'saida' ? "saída" : "entrada"}</button>
       </form>
     </TransactionsContainer>
   )
